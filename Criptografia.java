9d2a9b2c15ec41ec8cfa03372b010cd419c87f6a
Sâmela Hostins <samela@hostins.com.br>
2023-10-09 20:51:55 -0300

    divideEmBlocosDe16Bytes

--- Criptografia.java
+++ Criptografia.java
@@ -1,4 +1,44 @@
 // Autoras: Karoline, Maria Eduarda e Sâmela
 
+import java.util.Arrays;
+
 public class Criptografia {
+
+    /*
+     * 1° Verifica quantos blocos podem ser formados
+     * 2° É criada uma matriz para armazenar os blocos (pois vamos usar no aes dps)
+     * 3° Utiliza um for para copiar cada bloco para uma matriz,
+     * sendo que cada linha representará um bloco
+     */
+    public byte[][] divideEmBlocosDe16Bytes(byte[] arquivoEmBytes) {
+        // math ceil para arredondar para cima para caber todos os bytes
+        int qtdDeBlocos = (int) Math.ceil((double) arquivoEmBytes.length / 16);
+        byte[][] matrizDeBlocos = new byte[qtdDeBlocos][16];
+
+        if (qtdDeBlocos == 1) {
+            matrizDeBlocos[0] = addPKCS7Padding(matriz);
+        } else {
+            for (int i = 0; i < qtdDeBlocos; i++) {
+                int indiceInicial = i * 16;
+                int indiceFinal = Math.min(indiceInicial + 16, arquivoEmBytes.length);
+                matrizDeBlocos[i] = Arrays.copyOfRange(arquivoEmBytes, indiceInicial, indiceFinal);
+            }
+
+            // PKCS#7 apenas ao último bloco
+            int ultimoBloco = qtdDeBlocos - 1;
+            matrizDeBlocos[ultimoBloco] = addPKCS7Padding(matrizDeBlocos[ultimoBloco], 16);
+        }
+
+        return matrizDeBlocos;
+
+    }
+
+    // para ver se deu certo
+    public String lerArquivo(byte[][] matriz) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < matriz.length; i++) {
+            result.append("Bloco ").append(i).append(": ").append(Arrays.toString(matriz[i])).append("\n");
+        }
+        return result.toString();
+    }
 }
--- Main.java
+++ Main.java
@@ -1,28 +1,40 @@
 // Autoras: Karoline, Maria Eduarda e Sâmela
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Criptografia e = new Criptografia();
         ValidarEntradas validarEntradas = new ValidarEntradas();
         Scanner scanner = new Scanner(System.in);
 
-        // Exemplo: C:/Users/Acer/OneDrive/Documentos/AtividadeFundamentos.pdf
-        String arquivo = validarEntradas.obterCaminhoArquivoValido(scanner);
-        System.out.println("Caminho de arquivo a criptografar: " + arquivo);
+        // Exemplo: C:\Users\Acer\OneDrive\Documentos\teste.txt
+        String arquivoDeEntrada = validarEntradas.obterCaminhoArquivoValido(scanner);
+        System.out.println("Caminho de arquivo a criptografar: " + arquivoDeEntrada);
         System.out.println("");
 
         // Precisa validar do destino
-        String arquivoDeSaida = validarEntradas.obterNomeDoArquivoValido(scanner);
-        System.out.println("Nome do arquivo de destino: " + arquivoDeSaida);
-        System.out.println("");
+        // String arquivoDeSaida = validarEntradas.obterNomeDoArquivoValido(scanner);
+        // System.out.println("Nome do arquivo de destino: " + arquivoDeSaida);
+        // System.out.println("");
 
-        String chave = validarEntradas.getChaveValida(scanner);
-        System.out.println("Chave: " + chave);
-        System.out.println("");
-        scanner.close();
+        // // 20,1,94,33,199,0,48,9,31,94,112,40,59,30,100,248
+        // String chave = validarEntradas.getChaveValida(scanner);
+        // System.out.println("Chave: " + chave);
+        // System.out.println("");
+        // scanner.close();
+
+        // Leitura do arquivo, é para ele não jogar exceção, pois já foi verificado se
+        // está certo/existe
+        byte[] arquivoEmBytes = Files.readAllBytes(Path.of(arquivoDeEntrada));
+
+        byte[][] matriz = e.divideEmBlocosDe16Bytes(arquivoEmBytes);
+        String resultado = e.lerArquivo(matriz);
+        System.out.println(resultado);
 
     }
 
