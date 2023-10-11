49a50c03931a0a87e8adac3de95c3c7e8f33ff54
maduredes <meredes_@hotmail.com>
2023-10-11 00:06:05 -0300

    enviando metodo pkcs7

--- models/Criptografia.java
+++ models/Criptografia.java
@@ -10,6 +10,28 @@ public class Criptografia {
      * 3° Utiliza um for para copiar cada bloco para uma matriz,
      * sendo que cada linha representará um bloco
      */
+
+    private byte[] addPKCS7Padding_2(byte[] blocoPreencher,Integer quantidadeBytes){
+        //precisa preencher de acordo com a quantidade de bytes faltantes:
+        //por exemplo recebi um bloco de byte que tem preenchido 2 bytes e eu tenho um total de 16 bytes
+        //logo faltam 14
+        //mesmo que o ultimo bloco nao tenha bytes para ser preenchidos
+        // deverá ser criado um novo bloco de 16 bytes adicional
+        Integer quantidadeBytesPreenchidos=0;
+        for(int i=0;i<blocoPreencher.length;i++) {
+            if (blocoPreencher[i] != 0) {
+                quantidadeBytesPreenchidos++;
+            }
+        }
+        if(quantidadeBytesPreenchidos < quantidadeBytes) {
+            byte[] byteArray = new byte[quantidadeBytes - quantidadeBytesPreenchidos];
+            Arrays.fill(byteArray, (byte) 0xFF);
+            return byteArray;
+        }
+        byte [] byteArrayAdicional=new byte[16];
+        Arrays.fill(byteArrayAdicional,(byte) 0xFF);
+        return byteArrayAdicional;
+    }
     public byte[][] divideEmBlocosDe16Bytes(byte[] arquivoEmBytes) {
         // math ceil para arredondar para cima para caber todos os bytes
         int qtdDeBlocos = (int) Math.ceil((double) arquivoEmBytes.length / 16);
@@ -34,9 +56,6 @@ public class Criptografia {
 
     }
 
-    /*private byte[][]  addPKCS7Padding(){
-
-    }*/
     // para ver se deu certo
     public String lerArquivo(byte[][] matriz) {
         StringBuilder result = new StringBuilder();
