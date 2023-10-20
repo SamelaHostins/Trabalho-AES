6df0d271ca71304f9446cc010c98329c92c2c1e1
karolinecustodio <karolinecustodio7@gmail.com>
2023-10-20 20:45:59 -0300

    criacao metodo mixColumns, xor e galoi

--- models/entradaSaidas/FileManager.java
+++ models/entradaSaidas/FileManager.java
@@ -11,11 +11,12 @@ import java.util.Scanner;
 public class FileManager {
 	String data = null;
 
+	// metodo para criar o arquivo
 	public void criaArq(String fileName) {
 		try {
 			File myObj = new File(fileName);
 			if (myObj.createNewFile()) {
-				System.out.println("Arquvo criado: " + myObj.getName());
+				System.out.println("Arquivo criado: " + myObj.getName());
 			} else {
 				System.out.println("O arquivo já existe.");
 			}
@@ -25,6 +26,7 @@ public class FileManager {
 		}
 	}
 
+	// metodo para ler o arquivo 
 	public void leArq(String path, String text) {
 		try {
 			FileWriter fWriter = new FileWriter(path);
@@ -66,26 +68,33 @@ public class FileManager {
 		}
 	}
 
-	// verifica se o arquivo é binario ou o texto do arquivo
+	// verifica se o arquivo é binario
 	public boolean ehArqBinario(File file) throws FileNotFoundException, IOException {
 		FileInputStream in = new FileInputStream(file);
+		
+		// Obtém o tamanho do arquivo usado
 		int size = in.available();
 
+		// Limita o tamanho a ser lido para no máximo 1024 bytes (
 		if (size > 1024) {
 			size = 1024;
 		}
 
 		byte[] fileData = new byte[size];
 
+		// Lê os primeiros bytes do arquivo
 		in.read(fileData);
 		in.close();
 
 		int numberOfAsciiCharacters = 0;
 		int numberOfOtherCharacters = 0;
 
+		// analisa os bytes contidos para determinar se o arquivo é binário ou não 
+		
 		for (int i = 0; i < fileData.length; i++) {
 			byte b = fileData[i];
 			if (b < 0x09)
+				// indica que o arquivo é binário
 				return true;
 
 			if (b == 0x09 || b == 0x0A || b == 0x0C || b == 0x0D)
@@ -97,8 +106,10 @@ public class FileManager {
 		}
 
 		if (numberOfOtherCharacters == 0)
+			//indica que o arquivo não é binário 
 			return false;
 
+		// Se a porcentagem de caracteres for maior que 95%, o método retorna true, indicando que o arquivo é binário 
 		return 100 * numberOfOtherCharacters / (numberOfAsciiCharacters + numberOfOtherCharacters) > 95;
 	}
 }
